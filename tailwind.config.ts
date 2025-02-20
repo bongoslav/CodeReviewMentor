89285bc0ef67f7f15d5032f4be29fce41b32cce2
Borislav Borisov <beborisov99@gmail.com>
2025-02-20 22:39:42 +0200

    init app

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,16 @@
-import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
+import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./src/**/*.tsx"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
       },
     },
   },
