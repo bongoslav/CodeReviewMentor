47cb469753413f3676d5cb6baf9e3893f5068935
Borislav Borisov <beborisov99@gmail.com>
2025-03-02 18:01:32 +0200

    added index on createdAt

--- prisma/migrations/20250302155832_init/migration.sql
+++ prisma/migrations/20250302155832_init/migration.sql
@@ -3,6 +3,9 @@ CREATE TABLE "Submission" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "code" TEXT NOT NULL,
     "language" TEXT NOT NULL,
-    "feedback" TEXT NOT NULL,
+    "feedback" TEXT NOT NULL DEFAULT '',
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+-- CreateIndex
+CREATE INDEX "Submission_createdAt_idx" ON "Submission"("createdAt");
